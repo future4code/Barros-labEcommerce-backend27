fad6a2f09f8c18c626fcf617956f09796c161549
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2023-01-24 10:43:40 -0300

    Criação da tabela movie_charcters e JOIN com a tabela movie e charcter

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,7 @@ app.get("/character", getAllCharacters)
 app.put("/character", createCharacter)
 app.delete("/character/:id", deleteCharacter)
 
+// app.get("/movie/:id/characters") //para determinado filme o endpoint retorna todos os personagens (precisaria usar o JOIN aqui e connection.raw com a mesma query)
+
 // app.get("/movie")
 // app.delete("/movies")
